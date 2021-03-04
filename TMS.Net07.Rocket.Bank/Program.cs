d41ce571f68ea10a45f3e2a4563cb8556da82a86
Alesya <alesya.lis.1@mail.ru>
2021-03-05 00:29:25 +0300

    first commit

---
+++ TMS.Net07.Rocket.Bank/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TMS.Net07.Rocket.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Account("name", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeDeposit(100, DateTime.Now);
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(500, DateTime.Now);
+            Console.WriteLine(account.Balance);
+
+            // Test that the initial balances must be positive.
+            try
+            {
+                var invalidAccount = new Account("invalid", -100);
+            }
+            catch (ArgumentOutOfRangeException exp)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(exp.ToString());
+            }
+            // Test for a negative balance.
+            try
+            {
+                account.MakeWithdrawal(2000, DateTime.Now);
+            }
+            catch (InvalidOperationException exp)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(exp.ToString());
+            }
+
+        }
+    }
+}
